0b298244c901bfb85dee1e13809cb3e083b24834
Stacy <stacylin.sunny@gmail.com>
2025-02-02 21:02:14 +0800

    設定vite.config.js的base路徑+build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //設定產品(production)、開發中路徑(dev)
+  base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-week02/' : '/',
   plugins: [react()],
 })
